4114ad8146fcbba2d421ff05e9c8441f54647f03
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-22 16:46:15 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Script pour afficher les arguments de la ligne de commande ligne par ligne
+# Script pour lire et afficher les arguments saisis interactivement ligne par ligne
+
+# Lit la ligne complète saisie par l'utilisateur (sans invite)
+read -r input
 
 # Compteur pour numéroter les arguments
 count=1
 
-# Boucle à travers tous les arguments avec $@
-for arg in "$@"; do
+# Boucle à travers les mots de la saisie, en les traitant comme des arguments
+for arg in $input; do
     echo "Argument $count: $arg"
     ((count++))
 done
